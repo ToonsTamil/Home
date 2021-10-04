e083a0bdd7ab3be812bc8494dd7390c04108b50a
ToonsTamil <89298516+ToonsTamil@users.noreply.github.com>
2021-10-04 11:40:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var list=document.getElementById("list");
 var list_icon=document.getElementById("list-icon");
 var top_bar=document.getElementById("topbar");
 
+var tele=document.getElementById("tele");
+var search_icon=document.getElementById("search-icon");
+
 search.onclick=function() { search_bar.style.display="block";
                            close_search.style.display="block";
 search.style.display="none";                       
@@ -29,3 +32,38 @@ close_search.onclick=function (){
       close_search.style.display="none";
    }
   } 
+
+search_icon.onclick=function (){
+ var txt=document.getElementById("search-box").value;
+  search_bar.style.display="none";
+ close_search.style.display="none";
+
+  search.style.display="block";
+  if("pokemon".includes(txt.toLowerCase())){
+  open ("");
+     }
+  
+  else if("pokemon season 14".includes(txt.toLowerCase())){
+  open("");
+}
+  else if("pokemon season 15".includes(txt.toLowerCase())){
+  
+  open ("");
+}
+  else if("luca".includes(txt.toLowerCase())){
+    open ("");
+  
+  }
+  else if("raya the last dragon".includes(txt.toLowerCase())){
+    open("");
+  }
+  else{
+    alert ("Result Not Found");
+  }
+}
+
+tele.onclick=function (){
+  open('https://t.me/All_Toons_Tamil');
+  }
+
+
